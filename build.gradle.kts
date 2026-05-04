b0a58884ee267d5b578983562be85a407d798199
Andret2344 <andret2344@gmail.com>
2026-05-04 17:36:34 +0200

    Fixed config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,53 @@
 plugins {
-	java
-	idea
-	application
+    java
+    idea
+    application
 }
 
 repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation(libs.jda)
-	implementation(libs.gson)
-	// logging
-	implementation(libs.slf4j.api)
-	implementation(libs.log4j.slf4j.impl)
-	implementation(libs.log4j.core)
-	// testing
-	testImplementation(platform(libs.junit.bom))
-	testImplementation(libs.junit.jupiter)
-	testRuntimeOnly(libs.junit.platform.launcher)
+    mavenCentral()
 }
 
 application {
-	mainClass.set("eu.andret.ads.ferrio.Ferrio")
+    mainClass.set("eu.andret.ads.ferrio.FerrioBot")
+}
+
+dependencies {
+    implementation(libs.jda)
+    implementation(libs.gson)
+    // logging
+    implementation(libs.slf4j.api)
+    implementation(libs.log4j.slf4j.impl)
+    implementation(libs.log4j.core)
+    // testing
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+application {
+    mainClass.set("eu.andret.ads.ferrio.Ferrio")
 }
 
 tasks {
-	compileJava {
-		sourceCompatibility = JavaVersion.VERSION_25.toString()
-		targetCompatibility = JavaVersion.VERSION_25.toString()
-	}
+    compileJava {
+        sourceCompatibility = JavaVersion.VERSION_25.toString()
+        targetCompatibility = JavaVersion.VERSION_25.toString()
+    }
 
-	test {
-		useJUnitPlatform()
-	}
+    test {
+        useJUnitPlatform()
+    }
 
-	jar {
-		duplicatesStrategy = DuplicatesStrategy.WARN
+    jar {
+        duplicatesStrategy = DuplicatesStrategy.WARN
 
-		from({
-			configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
-		})
+        from({
+            configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+        })
 
-		manifest {
-			attributes["Main-Class"] = application.mainClass.get()
-		}
-	}
+        manifest {
+            attributes["Main-Class"] = application.mainClass.get()
+        }
+    }
 }
